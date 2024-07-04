ebfd49d2195797842e63e30de6a5bebb66e6715d
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-04 18:45:34 -0500

    add javacript v.1

---
+++ script.js
@@ -0,0 +1,72 @@
+// variables
+
+let workTitle = document.getElementById("work");
+let breakTitle = document.getElementById("break");
+
+let workTime = 1;
+let breakTime = 5;
+
+let seconds = "00";
+
+// display
+window.onload = () => {
+  document.getElementById("minutes").innerHTML = workTime;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  workTitle.classList.add("active");
+};
+
+// start timer
+function start() {
+  // change button
+  document.getElementById("start").style.display = "none";
+  document.getElementById("reset").style.display = "block";
+
+  // change the time
+  seconds = 59;
+
+  let workMinutes = workTime -1;
+  let breakMinutes = breakTime -1;
+
+  breakCount = 0;
+
+  // countdown
+  let timerFuncion = () => {
+    // change the display
+    document.getElementById("minutes").innerHTML = workMinutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // start
+    seconds -= 1;
+
+    if (seconds === 0) {
+      workMinutes -= 1;
+      if(workMinutes === -1) {
+        if(breakCount % 2 === 0) {
+          // start break
+          workMinutes = breakMinutes;
+          breakCount ++;
+
+          // change title
+          workTitle.classList.remove("active");
+          breakTitle.classList.add("active");
+        } else {
+          // continue work
+          workMinutes = workTime;
+          breakCount ++;
+
+          // change the painel
+          breakTitle.classList.remove("active");
+          workTitle.classList.add("active");
+        }
+      }
+
+
+
+      seconds = 59;
+    }
+  };
+
+  // start countdown
+  setInterval(timerFuncion, 1000);
+}
